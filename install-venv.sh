70d238e4cb7dc58e8ed7e86cc5a896fcef0ce216
Nicolas Ledez <github@ledez.net>
2023-12-25 11:12:07 +0000

    Update install-venv.sh

--- install-venv.sh
+++ install-venv.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 # set -e
-# echo "====== vim2"
-# VIM2=~/.vim/venv2
-# if [ -e ${VIM2} ]; then
-# 	${VIM2}/bin/python -V || rm -rf ${VIM2}
-# fi
-# if [ ! -e ${VIM2} ]; then
-# 	virtualenv -p $(which python) ${VIM2}
-# fi
 
 echo "====== vim3"
 VIM3=~/.vim/venv3
@@ -15,14 +7,11 @@ if [ -e ${VIM3} ]; then
 	${VIM3}/bin/python -V || rm -rf ${VIM3}
 fi
 if [ ! -e ${VIM3} ]; then
-	# virtualenv -p `ls -1rt /usr/local/Cellar/python/*/bin/python3 | tail -1` ${VIM3}
-	virtualenv -p `which python3` ${VIM3}
+	python3 -m venv ${VIM3}
 fi
 
 for v in 3; do
        echo "====== vim${v}"
-       # . ~/.vim/venv${v}/bin/activate
-       ~/.vim/venv${v}/bin/pip install --upgrade pip
-       ~/.vim/venv${v}/bin/pip install -r requirements.txt
-       # deactivate
+       ~/.vim/venv${v}/bin/python -m pip install -U pip setuptools
+       ~/.vim/venv${v}/bin/python -m pip install -r requirements.txt
 done
